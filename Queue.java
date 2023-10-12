3d7f3cc7aa38278d043df3d7a9d35fcf20171e2b
W Carr <carrwi96@students.rowan.edu>
2023-10-12 11:15:28 -0400

    copied queue for p2

---
+++ Queue.java
@@ -0,0 +1,83 @@
+
+/*
+ * Purpose: Data Structure and Algorithms Lab 6 Problem 2
+ * Status: Complete and thoroughly tested
+ * Last update: 10/12/22
+ * Submitted:  10/12/22
+ * Comment: test suite and sample run attached
+ * Comment: I declare that this is entirely my own work
+ * @author: William Carr
+ * @version: 2023.10.12
+ */
+
+public class Queue<T> implements QueueInterface<T> {
+
+    protected int numItems;
+    protected T[] items;
+    protected int back;
+    protected int front;
+
+    public Queue() {
+        items = (T[]) new Object[3];
+        numItems = front = back = 0; //multiple assignments
+    }
+
+    public void dequeueAll() {
+        items = (T[]) new Object[3];
+        numItems = front = back = 0;
+    }
+
+    public boolean isEmpty() {
+        return numItems == 0;
+    }
+
+    public T peek() throws QueueException {
+        if(numItems != 0)
+            return items[front];
+        else throw new QueueException("peek: queue is empty");
+    }
+
+    public void enqueue(T item) throws QueueException {
+        int itemsLen = items.length;
+        if(numItems == itemsLen) {
+            resize();
+            itemsLen = itemsLen << 1;
+        }
+        items[back] = item;
+        back = (back + 1) % itemsLen;
+        numItems++;
+    }
+
+    protected void resize() {
+        int itemsLen = items.length;
+        T[] temp = (T[]) new Object[itemsLen*2];
+        for(int i = 0; i < numItems; i++) {
+            temp[i] = items[(front+i) % itemsLen];
+        }
+        //after all this happens, front and back are messed up
+        this.front = 0;
+        this.back = numItems;
+        this.items = temp;
+    }
+
+    public T dequeue() throws QueueException {
+        if(numItems != 0) {
+            T result = items[front];
+            items[front] = null;
+            front = (front + 1) % items.length; //modulo removes comparison
+            numItems--;
+            return result;
+        }
+        else throw new QueueException("dequeue: queue is empty");
+    }
+
+    public String toString() {
+        int itemsLen = items.length;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < numItems; i++) {
+            sb.append(items[(front+i) % itemsLen] + " ");
+        }
+        return sb.toString();
+    }
+
+}
