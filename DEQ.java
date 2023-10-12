b25427f28be7136579636f7939dcf541ca21b2d7
W Carr <carrwi96@students.rowan.edu>
2023-10-12 04:08:30 -0400

    modified deq for formula to use modulo operator properly

--- DEQ.java
+++ DEQ.java
@@ -7,8 +7,9 @@ public class DEQ<T> extends QueueCRAB<T> implements ExtendedQueueInterface<T>{
 	//peek == peekFront
 
 	public T peekBack() {
+		int itemsLen = items.length;
 		if(numItems != 0)
-			return items[back-1];
+			return items[(itemsLen + back - 1) % itemsLen];
 		else throw new ExtendedQueueException("peek: DEQ is empty");
 	}
 
@@ -19,15 +20,16 @@ public class DEQ<T> extends QueueCRAB<T> implements ExtendedQueueInterface<T>{
 		int itemsLen = items.length;
 		if(numItems == itemsLen)
 			resize();
-		front = (front - 1) % itemsLen;
+		front = (itemsLen + front - 1) % itemsLen;
 		items[front] = item;
 	}
 
 	//dequeue == dequeueFront
 
 	public T dequeueBack() {
+		int itemsLen = items.length;
 		if(numItems != 0) {
-			back = (back - 1) % items.length; //modulo removes comparison
+			back = (itemsLen + back - 1) % itemsLen; //modulo removes comparison
 			T result = items[back];
 			items[back] = null;
 			numItems--;
